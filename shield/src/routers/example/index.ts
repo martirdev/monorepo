84bf1cf21034f08d6f55316d9c9532065472aaf9
Maksim Pushkarev <mimpushkarev@ya.ru>
2023-12-08 07:58:39 +0000

    feat: FRONTEND-24. Добавить шаблон фронта для trpc

--- shield/src/routers/example/index.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-import { tRPC } from "..";
-
-export const exampleRouter = tRPC.router({
-  getById: tRPC.procedure.input(z.string()).query(({ ctx, input }) => {
-    return input;
-  }),
-});
