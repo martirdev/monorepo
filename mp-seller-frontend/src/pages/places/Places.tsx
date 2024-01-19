03feaa79001c390fb9e473fa7c51c0d3a94eadf2
AnnaErs <84527125+AnnaErs@users.noreply.github.com>
2024-01-19 11:55:30 +0500

    FRONTEND-47: [frontend] Реализовать дроуеры добавления магазинов (#47)

--- mp-seller-frontend/src/pages/places/Places.tsx
+++ mp-seller-frontend/src/pages/places/Places.tsx
@@ -1,9 +1,34 @@
-import PlacesList from '_widgets/place-list';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Input } from 'antd';
+import { useCallback, useState } from 'react';
+
+import AddPlaceDrawer from '_widgets/places/place-drawer';
+import PlacesList from '_widgets/places/place-list';
+
+const {Search} = Input;
 
 const Places = function Places() {
+    const [openAddDrawer, setOpenAddDrawer] = useState(false);
+
+    const showAddDrawer = useCallback(() => {
+        setOpenAddDrawer(true);
+    }, []);
+
+    const onClose = useCallback(() => {
+        setOpenAddDrawer(false);
+    },[]);
+
     return (
-        <div>
+        <div className="mx-40 mt-16">
+            <div className="mb-14 flex justify-between">
+                <Search placeholder="здесь будет поиск, пока его нет :(" onSearch={() => {}} className="max-w-sm w-full" />
+                <Button type="primary" icon={<PlusOutlined />} onClick={showAddDrawer}>
+                    Добавить магазин
+                </Button>
+            </div>
             <PlacesList />
+
+            <AddPlaceDrawer onClose={onClose} open={openAddDrawer} title={'Добавление магазина'} />
         </div>
     );
 };
