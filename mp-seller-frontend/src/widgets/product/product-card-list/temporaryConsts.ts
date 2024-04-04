f1942b11cf1241a340788fc38303f85abc8ac3ad
AnnaErs <annaershova02@gamil.com>
2024-04-04 22:55:50 +0500

    FRONTEND-45: Изменение временных констант

--- mp-seller-frontend/src/widgets/product/product-card-list/temporaryConsts.ts
+++ mp-seller-frontend/src/widgets/product/product-card-list/temporaryConsts.ts
@@ -5,21 +5,21 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: 'e9458050-1c65-4d0f-8b45-dc9489305a7c',
         key: 'e9458050-1c65-4d0f-8b45-dc9489305a7c',
         articule: '9458050',
-        name: 'Lorem, ipsum dolor',
+        name: 'Лампа настольная с регулировкой яркости',
         marketplaces: [
             {
                 id: '1',
                 isSynchronized: true,
                 type: 'ym',
                 price: 5500,
-                name: 'Amet'
+                name: 'ЛампаДа'
             },
             {
                 id: '2',
                 isSynchronized: true,
                 type: 'ozon',
                 price: 5000,
-                name: 'Amet ozon'
+                name: 'LoveMe Shop'
             }
         ]
     },
@@ -27,14 +27,14 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: '4c2ac452-769e-479e-a376-ed9b56d8b8ff',
         key: '4c2ac452-769e-479e-a376-ed9b56d8b8ff',
         articule: '4251452',
-        name: 'Molestias, officia sequi',
+        name: 'Фонарик для охоты на оленей',
         marketplaces: [
             {
                 id: '1',
                 isSynchronized: true,
                 type: 'ym',
                 price: 3500,
-                name: 'Amet'
+                name: 'ЛампаДа'
             }
         ]
     },
@@ -42,14 +42,14 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: '8554ae64-8d91-427d-b2b5-d83e3c99cc05',
         key: '8554ae64-8d91-427d-b2b5-d83e3c99cc05',
         articule: '5549064',
-        name: 'Fuga, quae esse',
+        name: 'Средство для мытья пола с лимоном',
         marketplaces: [
             {
                 id: '1',
                 isSynchronized: true,
                 type: 'ozon',
                 price: 4000,
-                name: 'Amet ozon'
+                name: 'Бытовая химия на каждый день'
             }
         ]
     },
@@ -57,21 +57,21 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: 'b88e4c81-6f69-4c08-a1e5-c0110ffa9aa9',
         key: 'b88e4c81-6f69-4c08-a1e5-c0110ffa9aa9',
         articule: '8854181',
-        name: 'Molestias, ipsum qui',
+        name: 'Кроссовки беговые мужские',
         marketplaces: [
             {
                 id: '1',
                 isSynchronized: false,
                 type: 'ym',
                 price: 2500,
-                name: 'Amet'
+                name: 'Спортивные товары'
             },
             {
                 id: '2',
                 isSynchronized: true,
                 type: 'ym',
                 price: 2600,
-                name: 'Amet test'
+                name: 'Спортивные товары'
             }
         ]
     },
@@ -79,14 +79,14 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: '747df9ef-35c5-4061-aa9f-67b7a1c59909',
         key: '747df9ef-35c5-4061-aa9f-67b7a1c59909',
         articule: '475698f',
-        name: 'Ipsum, a autem',
+        name: 'Кот-батон плюшевый для детей',
         marketplaces: [
             {
                 id: '1',
                 isSynchronized: false,
                 type: 'ozon',
                 price: 200,
-                name: 'Amet ozon'
+                name: 'Детский мир'
             }
         ]
     },
@@ -94,14 +94,14 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: '27528a15-0cee-4336-aceb-aacae31cf609',
         key: '27528a15-0cee-4336-aceb-aacae31cf609',
         articule: '7528115',
-        name: 'Veniam, asperiores iure',
+        name: 'Набор акварельных цветных карандашей',
         marketplaces: [
             {
                 id: '1',
                 isSynchronized: true,
                 type: 'ym',
                 price: 400,
-                name: 'Amet test'
+                name: 'Канцтовары'
             }
         ]
     },
@@ -109,14 +109,14 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: '5e47c0fe-af40-47fc-8236-9b5ba58e9f5d',
         key: '5e47c0fe-af40-47fc-8236-9b5ba58e9f5d',
         articule: '9472043',
-        name: 'Eaque, assumenda rerum',
+        name: 'Набор сухоцветов: овёс, лаванда',
         marketplaces: [
             {
                 id: '1',
                 isSynchronized: false,
                 type: 'ym',
                 price: 1000,
-                name: 'Amet'
+                name: 'Цветы96'
             }
         ]
     },
@@ -124,14 +124,14 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: 'a66d9aaf-e1dd-4135-92c9-2adf207d38eb',
         key: 'a66d9aaf-e1dd-4135-92c9-2adf207d38eb',
         articule: '6629653',
-        name: 'Commodi, magni similique',
+        name: 'Морская соль обогащённая цинком',
         marketplaces: [
             {
                 id: '1',
                 isSynchronized: true,
                 type: 'ym',
                 price: 1100,
-                name: 'Amet'
+                name: 'Дары моря'
             }
         ]
     },
@@ -139,14 +139,14 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: 'e9754c23-1e1e-483b-a790-ecb1e71da52e',
         key: 'e9754c23-1e1e-483b-a790-ecb1e71da52e',
         articule: '9754823',
-        name: 'Deleniti, harum saepe',
+        name: 'Мультивитамины для женщин',
         marketplaces: [
             {
                 id: '1',
                 isSynchronized: true,
                 type: 'ym',
                 price: 5500,
-                name: 'Amet'
+                name: 'Бады для женщин'
             }
         ]
     },
@@ -154,14 +154,14 @@ export const TEMPOPARY_MOCK_PRODUCTS: Array<ProductType> = [
         id: '385c9bd0-3caa-4e7a-b597-37d538556315',
         key: '385c9bd0-3caa-4e7a-b597-37d538556315',
         articule: '8539230',
-        name: 'Reprehenderit, quo eum',
+        name: 'Коллаген для роста волос',
         marketplaces: [
             {
                 id: '2',
                 isSynchronized: true,
                 type: 'ym',
                 price: 5500,
-                name: 'Amet'
+                name: 'Бады для мужчин'
             }
         ]
     }
